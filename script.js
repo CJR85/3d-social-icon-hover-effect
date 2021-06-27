7ccd87a3c6bcb70fb2831ea288c636817d72dfd0
Calvin Roberts <roberts.calvin85@gmail.com>
2021-06-27 01:21:09 +0100

    Fix function on mouseleave.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let list = document.querySelectorAll('.sci li'),
 list.forEach((elements) => {
   elements.addEventListener('mouseenter', function (event) {
     let color = event.target.getAttribute('data-color');
-    bg.getElementsByClassName.backgroundColor = color;
+    bg.style.backgroundColor = color;
   });
   elements.addEventListener('mouseleave', function (event) {
     bg.style.backgroundColor = '#fff';
